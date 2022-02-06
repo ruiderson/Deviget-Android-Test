7fd6ab4aca5e601553dbb024d2b05299cd09e927
ruiderson <ruiderson@live.com>
2022-02-06 18:53:07 -0300

    Add the activity_main.xml with SlidingPaneLayout

---
+++ app/src/main/kotlin/com/ruiderson/deviget_android_test/main/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ruiderson.deviget_android_test.main
+
+import androidx.appcompat.app.AppCompatActivity
+import com.ruiderson.deviget_android_test.R
+
+internal class MainActivity : AppCompatActivity(R.layout.activity_main)
